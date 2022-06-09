e86c510ca3242e1d9473d1774dad83b523ef04ab
Luna Jiménez Fernández <luna.jimfer@gmail.com>
2022-06-09 18:12:38 +0200

    First implementation of the Replay Buffer

--- memories/replay_buffer.py
+++ memories/replay_buffer.py
@@ -11,7 +11,7 @@
 
 
 # IMPORTS #
-from typing import List, Any, Tuple, Union
+from typing import List, Any, Tuple, Union, Optional
 
 from memories import Experience, unpack_experiences
 
@@ -42,6 +42,8 @@ class Episode:
         List of final flags f (after episode completion)
     episode_reward: list[float]
         Total reward obtained by this episode / trajectory
+    rewards_to_go: List[float]
+        Rewards to go for each experience within the episode
     """
 
     # ATTRIBUTES #
@@ -63,6 +65,8 @@ class Episode:
     final_flags: List[bool]
     # Total reward obtained by this episode / trajectory, repeated for all experiences
     episode_reward: List[float]
+    # Rewards to go for each experience within the episode
+    rewards_to_go: List[float]
 
     # CONSTRUCTOR #
     def __init__(self):
@@ -136,4 +140,154 @@ class Episode:
 
 # REPLAY BUFFER
 class ReplayBuffer:
-    pass
+    """
+    A ReplayBuffer represents the memory buffer used by actor-critic reinforcement-learning methods, used
+    to store and handle the experiences from the agent episodes.
+
+    Unlike Replay Memory (used by value-based methods such as DQN), Replay Buffers work on-policy, by storing
+    experiences performed with the current policy and being flushed after use.
+
+    current_episode: Episode, optional
+        Current episode. If None, there is no currently started episode
+    episode_list: List[Episode]
+        List of all episodes, not including the current episode
+    states: list[Any]
+        List of current states s of ALL episodes
+    actions: list[int or tuple]
+        List of actions a of ALL episodes
+    rewards: list[float]
+        List of rewards r of ALL episodes
+    next_states: list[Any]
+        List of next states s' of ALL episodes
+    final_flags: list[bool]
+        List of final flags f of ALL episodes
+    episode_reward: list[float]
+        Total reward for each episode, for ALL episodes
+        Note: experiences of the same episode have the same reward
+    rewards_to_go: List[float]
+        Rewards to go for each experience within the episode, for ALL episodes
+    """
+
+    # ATTRIBUTES #
+
+    # Current episode. If None, there is no currently started episode
+    current_episode: Optional[Episode]
+    # List of all episodes, not including the current episode
+    episode_list: List[Episode]
+
+    # List of current states s of ALL episodes
+    states: List[Any]
+    # List of actions a of ALL episodes
+    actions: List[Union[int, Tuple]]
+    # List of rewards r of ALL episodes
+    rewards: List[float]
+    # List of next states s' of ALL episodes
+    next_states: List[Any]
+    # List of final flags f of ALL episodes
+    final_flags: List[bool]
+    # Total reward for each episode, for ALL episodes
+    # Note: experiences of the same episode have the same reward
+    episode_reward: List[float]
+    # Rewards to go for each experience within the episode, for ALL episodes
+    rewards_to_go: List[float]
+
+    # CONSTRUCTOR #
+    def __init__(self):
+
+        # The Replay Buffer starts without a current episode
+        self.current_episode = None
+
+        # All lists start empty
+        self.episode_list = []
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.next_states = []
+        self.final_flags = []
+        self.episode_reward = []
+        self.rewards_to_go = []
+
+    # METHODS #
+
+    # Episode management
+    def new_episode(self):
+        """
+        Creates a new episode and sets it as the current episode
+        """
+
+        # Ignore this method if an episode already exists
+        if not self.current_episode:
+            self.current_episode = Episode()
+
+    def finish_episode(self, completed):
+        """
+        Finishes the episode, removing it as the current episode and storing the episode values
+        within the buffer for faster usage
+
+        If the episode has been "cut out" (the episode is not actually finished but the training process stops
+        earlier due to cut off), the final experience is not marked as final
+
+        Parameters
+        ----------
+        completed: bool
+            True if the episode has finished naturally, False otherwise
+        """
+
+        # Ignore this method if there is no current episode
+        if self.current_episode:
+
+            # Mark the episode as finished
+            self.current_episode.finish_episode(completed)
+
+            # Store the episode info within the buffer lists
+            self.states.extend(self.current_episode.states)
+            self.actions.extend(self.current_episode.actions)
+            self.rewards.extend(self.current_episode.rewards)
+            self.next_states.extend(self.current_episode.next_states)
+            self.final_flags.extend(self.current_episode.final_flags)
+            self.episode_reward.extend(self.current_episode.episode_reward)
+            self.rewards_to_go.extend(self.current_episode.rewards_to_go)
+
+            # Store the current episode within the buffer and remove it from the current episode variable
+            self.episode_list.append(self.current_episode)
+            self.current_episode = None
+
+    # Experience management
+    def insert_experience(self, state, action, reward, next_state):
+        """
+        Inserts an experience into the current episode. Experiences are, by default, unfinished.
+
+        Parameters
+        ----------
+        state: Any
+            Initial state s
+        action: int
+            Action a performed in state s
+        reward: float
+            Reward r obtained after performing action a in state s
+        next_state: Any
+            Next state s' reached after applying action a to state s
+        """
+
+        # Ignore this method if there is no current episode
+        if self.current_episode:
+            self.current_episode.insert_experience(state, action, reward, next_state)
+
+    # Replay buffer management
+    def empty(self):
+        """
+        Flushes the replay buffer to empty the information, preparing it for the next training epoch
+        """
+        
+        # The Replay Buffer starts without a current episode
+        self.current_episode = None
+
+        # All lists start empty
+        self.episode_list = []
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.next_states = []
+        self.final_flags = []
+        self.episode_reward = []
+        self.rewards_to_go = []
